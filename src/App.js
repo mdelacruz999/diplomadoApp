3114a79bd974e75bc4c104d301fd833e858c782e
mdelacruz999 <delacruzmoises80@gmail.com>
2022-10-15 23:22:12 -0500

    commit 2

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import formulario from './components/formulario.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <formulario/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/formulario.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const formulario = () => {
+    const [fruta,setFruta] = useState('');
+    const [descripcion,setDescripcion] = useState('');
+  return (
+    <div className='container mt-5'>
+        <h1 className='text-center'>Crud Basico</h1>
+        <hr>
+        <form>
+            <input 
+            className='form-control mb-2'
+            type='text'
+            placeholder='Ingrese Fruta'
+            ></input>
+            <input 
+            className='form-control mb-2'
+            type='text'
+            placeholder='Ingrese Descripción'
+            ></input>
+            <button>Agregar</button>
+        </form>
+        </hr>
+        
+    </div>
+  )
+}
+
+export default formulario
